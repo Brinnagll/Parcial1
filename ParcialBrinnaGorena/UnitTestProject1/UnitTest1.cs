1d5f081e21698074f1017ca96284ded59a7000bd
Brinna Francesca Gorena Llado <a2019119323@estudiantes.upsa.edu.bo>
2020-09-19 14:54:22 -0400

    parcial completo

---
+++ ParcialBrinnaGorena/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialBrinnaGorena.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGet()
+        {
+            //Arrange
+            CountriesController countriesController = new CountriesController();
+
+
+            //Act
+            var listaP = countriesController.GetCountries();
+
+            //Assert
+            Assert.IsNotNull(listaP);
+        }
+    }
+}
